5d9ed8f23a758e9f3c5a44eaa00dbce5fa671035
Andrew Paulikas <APaulikas@digitaldogdirect.com>
2020-07-01 14:40:27 -0400

    created askForBoardSize

--- Main/game/UserInterface.py
+++ Main/game/UserInterface.py
@@ -1,8 +1,24 @@
+import sys
+
 class UserInterface:#use this to run the game in CMD
+
         def updateBoard(gBoard):#displays board to player
             gBoard.toString()
 
-        def askForInput():#use this anytime input is needed from the player
+        def askForBoardSize():
+            #error handling
+            boardSize = 0
+            while boardSize < 1:
+                try:
+                    boardSize = int(input("please enter the board size"))
+                except:
+                    print("Please Enter a Positve number")
+            return boardSize
+
+        def askForMines():
+            pass
+
+        def askForCords():
             pass
 
         def help():#should print out an explanation of the rules and how to play
--- Main/main.py
+++ Main/main.py
@@ -2,5 +2,5 @@ from game.Board import *
 from game.Rules import *
 from game.UserInterface import *
 
-gBoard = Board(6)
+gBoard = Board(UserInterface.askForBoardSize())
 UserInterface.updateBoard(gBoard)
