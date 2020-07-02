4cd9d9a2ef3468afdc32fa2490793e4e014304fe
Andrew Paulikas <APaulikas@digitaldogdirect.com>
2020-07-02 10:14:11 -0400

    Added Dev Mode

--- Main/game/Rules.py
+++ Main/game/Rules.py
@@ -1,8 +1,15 @@
 class Rules:
+    win = False
+    loss = False
+
+    def __init__(self, gBoard, rBoard):
+        self.gBoard = gBoard
+        self.rBoard = rBoard
+
     def generateGame():#fill the refrence board with mines and number of surrounding mines
         pass
-    def checkGameState(gBoard,rBoard):#checks to see if the player has won or lost
-        return True
+    def checkGameState(self):#checks to see if the player has won or lost
+        pass
     def checkForWin():
         pass
     def checkForLoss():
--- Main/main.py
+++ Main/main.py
@@ -1,21 +1,31 @@
 from game.Board import *
 from game.Rules import *
 from UI.UserInterface import *
-
+#ask the user for game setup
 boardSize = UserInterface.askForBoardSize()
 numMines = UserInterface.askForMines(boardSize * boardSize)
-playGame = True
-
+#setup the boards
 gBoard = Board(boardSize)
 rBoard = Board(boardSize)
 rBoard.addMine(numMines)
-
-while playGame:
-    Rules.checkGameState(gBoard,rBoard)
-    print("game board")
-    UserInterface.updateBoard(gBoard)
-    print("Refrence board")
-    UserInterface.updateBoard(rBoard)
-    playGame = UserInterface.userInput(gBoard, rBoard)
-
+#setup the Rules
+rules = Rules(gBoard, rBoard)
+#additional variables
+playGame = True
+devMode = False
+#main game loop
+if devMode:
+    while playGame:
+        print("game board")
+        UserInterface.updateBoard(gBoard)
+        print("Refrence board")
+        UserInterface.updateBoard(rBoard)
+        playGame = UserInterface.userInput(gBoard, rBoard)
+        rules.checkGameState()
+else:
+    while playGame:
+        UserInterface.updateBoard(gBoard)
+        playGame = UserInterface.userInput(gBoard, rBoard)
+        rules.checkGameState()
+#game over
 print("Thanks for Playing")
